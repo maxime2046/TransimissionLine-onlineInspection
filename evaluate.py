12c61fc080bbc6c1999ebb9e262f707996dd0c92
maxime2046 <liuyue2046@126.com>
2018-12-08 15:11:32 +0800

    Add files via upload
    （1）添加图像总数统计、预测图像总数统计与两者相等校 验；
    （2）增加隐患图像数目统计，根据标准修改漏检率计算：漏检/隐患总数 
     （3）删除 代码中部分重复取值代码

--- evaluate.py
+++ evaluate.py
@@ -61,50 +61,24 @@ def readResult(resultFile):
         temp_result['image_name'] = filename
 
         flags = result_iter.get('flag')
-        temp_result['fault'] = flags
-
-        #size_iter = result_iter.find('size')
-        #size = getSize(size_iter)
-       # temp_result['size'] = size
-
-
-        for object_iter in result_iter.findall('object'):
-            object_name = object_iter.get('name')
+        if (flags == 'true' or flags == 'false' or flags == 'True' or flags == 'False'):
+            temp_result['fault'] = flags.title()
+            for object_iter in result_iter.findall('object'):
+                object_name = object_iter.get('name')
             #temp_result['object_name'] = object_name
             #object_name = list(set(pred_result['object_name']))
-            bndbox = object_iter.find("bndbox")            
-            object_location = readShape(bndbox)
-            temp_result.setdefault('object_name',[]).append(object_name)
-            temp_result.setdefault(object_name,[]).append(object_location)
-        result.append(temp_result)
+                bndbox = object_iter.find("bndbox")            
+                object_location = readShape(bndbox)
+                temp_result.setdefault('object_name',[]).append(object_name)
+                temp_result.setdefault(object_name,[]).append(object_location)
+            result.append(temp_result)
+            
+        else:
+            print ('the result flag is wrong of %s',flags)
+            result = []
+            return result
+                    
     return result
-
-def label_save_img(img_file,pred_result,img_labeled_save_path,pre_name):
-    
-    image_name = pred_result['image_name']
-    im = image_name.split('.')
-    img_name = im[0]
-
-    #img_name = pred_result['image_name']
-    img_full_name = os.path.join(img_file,img_name + '.jpg')
-    img = cv2.imread(img_full_name)
-    color_white = (255,255,255)
-    #create the labeled image sava name:'E:\\img_result\\192.3.1.03lsjljl.jpg'
-    save_name = os.path.join(img_labeled_save_path,pre_name+image_name + '_result.jpg')
-    #pre_labeled_img_save_file = 
-    if pred_result.has_key('object_name'):
-            object_name = list(set(pred_result['object_name']))
-            for  obj_name in object_name:
-                color = (random.randint(0,256),random.randint(0,256),random.randint(0,256))
-                dets = pred_result[obj_name]
-                for det in dets:
-                    bbox = det[:4]
-                    bbox = map(int,bbox)
-                    cv2.rectangle(img,(bbox[0],bbox[1]),(bbox[2],bbox[3]),color = color,thickness = 2)
-                    cv2.putText(img,'%s' %( obj_name),(bbox[0],bbox[1]+10),color = color_white,fontFace = cv2.FONT_HERSHEY_COMPLEX,fontScale=0.5)
-    
-    cv2.imwrite(save_name,img)
-
 
 def readVocXml(xmlfile,image_name):
     parser = etree.XMLParser(encoding=ENCODE_METHOD)
@@ -134,6 +108,45 @@ def readVocXml(xmlfile,image_name):
     return temp_result
 
 
+
+def label_save_img(img_file,pred_result,ground_truth_result,img_labeled_save_path,pre_name):
+    
+    image_name = pred_result['image_name']
+    im = image_name.split('.')
+    img_name = im[0]
+
+    #img_name = pred_result['image_name']
+    img_full_name = os.path.join(img_file,img_name + '.jpg')
+    img = cv2.imread(img_full_name)
+    color_white = (255,255,255)
+    #create the labeled image sava name:'E:\\img_result\\192.3.1.03lsjljl.jpg'
+    save_name = os.path.join(img_labeled_save_path,pre_name+image_name + '_result.jpg')
+    #pre_labeled_img_save_file = 
+    if pred_result.has_key('object_name'):
+            object_name = list(set(pred_result['object_name']))
+            for  obj_name in object_name:
+                color = (random.randint(30,200),random.randint(30,200),random.randint(30,200))
+                dets = pred_result[obj_name]
+                for det in dets:
+                    bbox = det[:4]
+                    bbox = map(int,bbox)
+                    cv2.rectangle(img,(bbox[0],bbox[1]),(bbox[2],bbox[3]),color = color,thickness = 2)
+                    cv2.putText(img,'%s' %( obj_name),(bbox[0],bbox[1]+10),color = color_white,fontFace = cv2.FONT_HERSHEY_COMPLEX,fontScale=0.5)
+    if ground_truth_result.has_key('object_name'):
+            object_name = list(set(ground_truth_result['object_name']))
+            for  obj_name in object_name:
+                color = (0,0,255)
+                dets = ground_truth_result[obj_name]
+                for det in dets:
+                    bbox = det[:4]
+                    bbox = map(int,bbox)
+                    cv2.rectangle(img,(bbox[0],bbox[1]),(bbox[2],bbox[3]),color = color,thickness = 2)                   
+    
+    cv2.imwrite(save_name,img)
+
+
+
+
 def readGroudTruth(groundTruthFile,image_name):
     image_name = image_name + ".xml"
     ground_image_file = os.path.join(groundTruthFile,image_name)
@@ -156,8 +169,9 @@ def evaluate(groundTruthFile,resultPath,excel_save_path,result_txt_save_path,img
     label_len = len(class_name)
 
     acc_txt_file = open(result_txt_save_path,'a+')
+    total_img_num = len(os.listdir(groundTruthFile))/2
         
-    for xmlFile in os.listdir(resultPath):
+    for xmlFile in os.listdir(resultPath):  #根据结果路径，遍历所有的xml文件（文件个数即参加比赛厂家个数）
         try:
             resultFile = resultPath+xmlFile
             xml_name = xmlFile.split('.xml')
@@ -165,7 +179,9 @@ def evaluate(groundTruthFile,resultPath,excel_save_path,result_txt_save_path,img
             precdict_true_count = [0,0] #first is true ,second is false
             predict_true,predict_false,groundtruth,accuracy = {},{},{},{}
             no_fault_img_num,fault_no_img_num,acc_mean, = 0.0 , 0.0 , 0.0
-            count = 0
+            #count = 0
+            ground_fault_img_num = 0
+            ground_normal_img_num = 0
             '''
             #img_labeled_save_path = os.path.join(img_labeled_save_path,onle_name)
             #img_labeled_save_path = img_labeled_save_path + '\\'
@@ -175,7 +191,9 @@ def evaluate(groundTruthFile,resultPath,excel_save_path,result_txt_save_path,img
             #get detection result and sava as a list
             pred_result = readResult(resultFile)
             #pred_count,gt_count,hit_count  = {},{},{}
-            total_img_num = len(pred_result)
+            total_pred_img_num = len(pred_result)
+            if(total_pred_img_num != total_img_num):
+                print ('the pred image num is %d not equal to the total image num %d'%(total_pred_img_num,total_img_num))
 
 
             #初始化识别结果excel表格
@@ -188,18 +206,19 @@ def evaluate(groundTruthFile,resultPath,excel_save_path,result_txt_save_path,img
                 name = row0[i-1]
                 table.write(0,i,name)
             #read ground label based on predict image_name and count the number of object prediction
-            for pred_result_list in pred_result:
+            for pred_detect_result in pred_result:
             
                 #paint the prediction to image and save
-                pre_labeled_img = label_save_img(groundTruthFile,pred_result_list,img_labeled_save_path,xml_only_name)
                 #pre_labeled_img_save_file = os.path.join(img_labeled_save_path,xml_only_name+image_name + '_result.jpg')
                 #cv2.imwrite(pre_labeled_img_save_file,pre_labeled_img)
 
-                imag_name = pred_result_list['image_name']
+                imag_name = pred_detect_result['image_name']
                 img_name = imag_name.split('.')
                 image_name = img_name[0]
                 #image_name =  pred_result_list['image_name']       
                 ground_truth_result = readGroudTruth(groundTruthFile,image_name)
+                pre_labeled_img = label_save_img(groundTruthFile,pred_detect_result,ground_truth_result,img_labeled_save_path,xml_only_name)
+
                 '''
                 #print (type(pred_result_list['fault']))
                 #print (pred_result_list['fault'])
@@ -210,13 +229,15 @@ def evaluate(groundTruthFile,resultPath,excel_save_path,result_txt_save_path,img
                 #count normal image but predict as fault
                 if (ground_truth_result['fault'] == 'False'):
                     no_fault_img_num = no_fault_img_num +1
-                    if (pred_result_list['fault'] == 'false'):
-                        no_fault_img_num = no_fault_img_num - 1                    
+                    ground_normal_img_num = ground_normal_img_num + 1  #统计测试数据集中正常的图像个数
+                    if (pred_detect_result['fault'] == 'False'):
+                        no_fault_img_num = no_fault_img_num - 1     #将正常的图像预测为带隐患的图像 数量                  
                 #count fault image but predict as normal
                 if (ground_truth_result['fault'] == 'True'):
                     fault_no_img_num = fault_no_img_num + 1 
-                    if (pred_result_list['fault'] == 'true'):
-                        fault_no_img_num = fault_no_img_num - 1 
+                    ground_fault_img_num = ground_fault_img_num + 1  #统计测试数据集中含有隐患的图像个数
+                    if (pred_detect_result['fault'] == 'True'):
+                        fault_no_img_num = fault_no_img_num - 1     #将含有隐患的图像预测为不带隐患的图像 数量
                     
                 '''         
                 #count normal image but predict as fault
@@ -226,14 +247,14 @@ def evaluate(groundTruthFile,resultPath,excel_save_path,result_txt_save_path,img
                 if (pred_result_list['fault'] == 'false') and (ground_truth_result['fault'] == 'True'):
                     fault_no_img_num = fault_no_img_num + 1 
                 '''
-                #count the total objects of ground label xml
+                #count the total objects of every fault based on ground_labeled xml
                 for key , value in ground_truth_result.items():
                     if key != 'fault':
                         groundtruth[key] = groundtruth.get(key,0) + len(value)
         
-                pred_detect_result = pred_result[count] #extract the count result
-                count += 1
-                #tabel = write(table,pred_detect_result,ground_truth_result)
+                #pred_detect_result = pred_result[count] #extract the count result
+                #count += 1
+                ##tabel = write(table,pred_detect_result,ground_truth_result)
                 write(table,pred_detect_result,ground_truth_result)
        
                 if pred_detect_result.has_key('object_name'):
@@ -248,7 +269,7 @@ def evaluate(groundTruthFile,resultPath,excel_save_path,result_txt_save_path,img
                             if pred_detect_result.has_key(obj_nam): # some object is in ground xml 
                                 pre_obj_loc = pred_detect_result[obj_nam]            
                                 grd_obj_loc = ground_truth_result[obj_nam]
-                                grd_obj_num = len(grd_obj_loc)
+                                #grd_obj_num = len(grd_obj_loc)
                                 #groundtruth[obj_nam] = groundtruth.get(obj_nam,0) + grd_obj_num                
             
                                 for grd_obj in grd_obj_loc:
@@ -265,15 +286,15 @@ def evaluate(groundTruthFile,resultPath,excel_save_path,result_txt_save_path,img
                                         #predict_false[obj_nam] = predict_false.get(obj_nam,0) + 1 
                             #else:# some object is in ground xml 
                                  #predict_false[obj_nam] = predict_false.get(obj_nam,0) + len(pre_obj_loc)
-            total_img_num = 100
-            miss_detect_acc = float(fault_no_img_num)/total_img_num
-            
-
-            wrong_detect_acc = float(no_fault_img_num)/total_img_num
 
             file.save(file_save_path)
+            
+            '''**********************caculate the evaluating indicator **************************'''
+            miss_detect_acc = float(fault_no_img_num)/ground_fault_img_num
+            wrong_detect_acc = float(no_fault_img_num)/total_img_num           
 
-            label_len = 0 
+            label_len = 0
+            #单独计算每类隐患的识别准确率  统计测试集中缺陷的类型数目
             for key,value in groundtruth.items():
                 if key != 'object_name' and key != 'image_name':
                     accuracy[key] = 0.0
@@ -297,7 +318,7 @@ def evaluate(groundTruthFile,resultPath,excel_save_path,result_txt_save_path,img
                     #print (predict_true[key],'   ', groundtruth[key], '   ',accuracy[key])
                     print ('the {} object detect accuracy is {:4f} '.format(key ,acc) )
             '''
-
+            #计算隐患识别的平均准确率
             acc_mean = acc_mean/label_len;
             print('the miss detect acc is:{:4f}'.format(miss_detect_acc))
             print('the wrong detect acc is:{:4f}'.format(wrong_detect_acc))
